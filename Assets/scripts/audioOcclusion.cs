df3b0fc8e6ab1d598a46a3d2df51343b26f710bb
XC3399 <113364159+XC3399@users.noreply.github.com>
2023-10-30 12:49:47 +0000

    started bolt audio occlusion

---
+++ Assets/scripts/audioOcclusion.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioOcclusion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Physics.Raycast(transform.position,)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
